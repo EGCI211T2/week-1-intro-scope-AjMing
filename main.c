c5ef0da2472f4807444ed60fec2bacfcd4f24a64
AjMing <62439174+AjMing@users.noreply.github.com>
2025-01-15 03:58:10 +0000

    add soda

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*******************************************************************************/
 #include <stdio.h>
 
-void useLocal();
+void useLocal()
 void useStaticLocal();
 void useGlobal();
 
